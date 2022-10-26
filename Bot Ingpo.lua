0cf0404e9557ebb09f47501d27078d320bdf2aca
Upilnamrud <116701819+UpilNamrud@users.noreply.github.com>
2022-10-26 18:37:41 +0700

    Add files via upload

---
+++ Bot Ingpo.lua
@@ -0,0 +1,23 @@
+Rdp = {
+    {nordp = 1, botname = "Bomah", Status = "Nonaktif"},
+    {nordp = 1, botname = "Jawsa", Status = "Nonaktif"},
+    {nordp = 2, botname = "kazyuuu", Status = "Nonaktif"},
+    {nordp = 2, botname = "zoruy", Status = "Nonaktif"},
+    {nordp = 3, botname = "Tarfi", Status = "Nonaktif"},
+    {nordp = 3, botname = "Bayai", Status = "Nonaktif"}
+}
+
+ShowIP = false
+ip_add = {
+
+}
+
+hookURL = "https://discord.com/api/webhooks/1034699929433686037/n4vWnKJEDX_eKJZJGsOFGr1yr6WDE3Xvzw3NPBRiCtITW03UPl0E4MqElHHx8UuqwcXr"
+
+Teks_Deskripsi = "List Babu"
+Teks_Footer = "Golek Duit Kanggo Masa Depan"
+
+Emote_Offline = "<:offline:982957367392088124>"
+Emote_Online = "<:online:982957403756716052>"
+
+menit = 5 --TIAP BERAPA MENIT BOTNYA NGIRIM WEBHOOK
